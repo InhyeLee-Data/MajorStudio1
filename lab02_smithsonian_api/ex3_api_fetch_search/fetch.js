4823577ab8a48bcf5d5f69c6461116b1b4a381b7
Inhye <54472616+InhyeLee-Data@users.noreply.github.com>
2020-09-14 01:19:16 +0900

    Update fetch.js

--- lab02_smithsonian_api/ex3_api_fetch_search/fetch.js
+++ lab02_smithsonian_api/ex3_api_fetch_search/fetch.js
@@ -28,12 +28,14 @@ function fetchSearchData(searchTerm) {
 
 fetchSearchData(search); 
 
-/* My Questions
+/* My Questions 
 1. What is unit_code: "FSG" ?
--> "unit_code" is one of the term categories:
-   culture, data_source, date, object_type, online_media_type, place, topic, unit_code
+FSG stands for Freer Gallery of Art and Arthur M. Sackler Gallery.
+-> "unit_code" is one of the term categories (culture, data_source, date, object_type, online_media_type, place, topic, unit_code).
 -> It looks to be part of response.rows[index].id 
+-> "unit_code" is the museum acronym.
 
-2. What is the best way to include a search with topics? There are 44680 topics.
+
+2. What is the best way to include a search with topics, when there are 44680 topics.
 
 */
