fd50f0d9c10f9dc334a3f59d356c6a283abffae2
Inhye <54472616+InhyeLee-Data@users.noreply.github.com>
2020-09-13 23:42:58 +0900

    Add files via upload

---
+++ lab02_smithsonian_api/ex1_api_fetch_object/fetch.js
@@ -0,0 +1,42 @@
+// Smithsonian API example code
+// check full API documentation here: https://edan.si.edu/openaccess/apidocs/
+
+
+// put your API key here;
+const apiKey = "6g9qUgS89puB1XZMgn5sNjRZf8K5xmhCjgaS2tL3";  
+
+// Access to individual objects by ID
+const objectBaseURL = "https://api.si.edu/openaccess/api/v1.0/content/";
+
+//fetches content based on id of an object.
+function fetchContentDataById(id) {
+  let url = objectBaseURL + id + "?api_key="+apiKey;
+  window
+  .fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log("Here's the content data of the specified object:", data.response);
+  
+    const source = data.response.content.descriptiveNonRepeating.data_source;
+    const title = data.response.content.descriptiveNonRepeating.title.content;
+    let imageLink = data.response.content.descriptiveNonRepeating.online_media.media[0].content;
+    const image = document.createElement('img');
+    image.src = imageLink;
+    image.width = 600;
+    document.querySelector('body').appendChild(image);
+    console.log(imageLink);
+    console.log("Source: " + source);
+    console.log("Title: " + title);
+    
+  })
+  .catch(error => {
+    console.log(error);
+  })
+}
+
+fetchContentDataById("edanmdm:npg_NPG.87.123"); // Gertrude Stein Charcoal Drawing
+
+// Task 1: Find a different object on https://collections.si.edu/search/ and retrieve the data with the code above
+// Make sure to check the box "Only return results with CC0 media" when searching
+// Task 2: Write the result into a variable and explore different variables through the Console
+
